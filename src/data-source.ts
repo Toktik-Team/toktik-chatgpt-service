4824c20fcb948cc82cd568f68a56f985c17f8a50
nicognaw <nicognaw@outlook.com>
2023-02-16 08:05:59 +0800

    Chore: Data source & schema definition.

---
+++ src/data-source.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { ChatGPTSession } from './gpt/gpt.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  synchronize: true,
+  logging: false,
+  entities: [ChatGPTSession],
+  migrations: [],
+  subscribers: [],
+});
+
+// to initialize initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch((error) => console.log(error));
